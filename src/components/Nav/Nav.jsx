f53acadeaa0dfd2476b3e0797e1bf475bc0cb890
3443ozan <vahidozan121@gmail.com>
2023-03-25 13:50:16 +0400

    done2

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Nav.scss'
+import kar from './icons/icon1.svg'
+import nar from './icons/icon2.svg'
+import ran from './icons/icon3.svg'
+import rak from './icons/icon4.svg'
 const Nav = () => {
   return (
     <nav>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><img src={kar} alt="" /></li>
+            <li id='speciel'><img src={nar} alt="" /></li>
+            <li><img src={ran} alt="" /></li>
+            <li><img src={rak} alt="" /></li>
         </ul>
     </nav>
   )
