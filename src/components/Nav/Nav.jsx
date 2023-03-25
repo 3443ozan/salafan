2c9e2274ce34d710b6ff1424fbe28eb3c0a88cc2
3443ozan <vahidozan121@gmail.com>
2023-03-25 13:18:28 +0400

    done 1

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav.scss'
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../components/Nav/Nav'
+import '../GlobalStyle.scss'
+const MainPage = () => {
+  return (
+    <div className="container">
+        <div className="Hero">
+            <Nav></Nav>
+        </div>
+    </div>
+  )
+}
+
+export default MainPage
